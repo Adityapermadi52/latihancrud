f0c0be7b2983b63c92051f3e1b72d7dd87df91c5
unknown <aditya.permadi@undiksha.ac.id>
2021-03-29 11:11:59 +0800

    laravel

---
+++ database/migrations/2021_03_27_061838_create_kegiatans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKegiatansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kegiatans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('Nama');
+            $table->date('waktu');
+            $table->string('gambar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kegiatans');
+    }
+}
